4f43539bbd9c4797a37c646bc4fa316d65ca2a18
Tucker MacIntyre <tmacinty@mail.umw.edu>
2019-01-31 23:23:36 -0500

    Final Commit

--- IntroToProgramming/Calculator_MacIntyre.py
+++ IntroToProgramming/Calculator_MacIntyre.py
@@ -12,13 +12,13 @@ z = 3
 #List containing 5 elements to alter, could have also just created an empty list
 numList = [0,0,0,0,0]
 
-numList[0] = x+y
-numList[1] = (y*z) + (3*x)
+numList[0] = x + y
+numList[1] = (y * z) + (3 * x)
 numList[2] = numList[0]**2
-numList[3] = ((2 * numList[1] -  (x / 2)) / numList[0])
+numList[3] = ((2 * numList[1]) -  (x / 2)) / numList[0]
 numList[4] = 7 % 3
 numList[2] = numList[2] + 3
-numList[4] = numList[4] * (3/4)
+numList[4] = numList[4] * (3 / 4)
 totalSum = sum(numList)
 
 #rounded to 2 decimal points
